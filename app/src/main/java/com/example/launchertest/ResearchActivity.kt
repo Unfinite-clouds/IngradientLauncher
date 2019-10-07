920726f4580e1bc134a6017ce36bb71e2a6f5c97
Max <poldersen2@gmail.com>
2019-10-07 04:01:44 +0300

    Research RecyclerView.notify*

--- app/src/main/java/com/example/launchertest/ResearchActivity.kt
+++ app/src/main/java/com/example/launchertest/ResearchActivity.kt
@@ -2,25 +2,52 @@ package com.example.launchertest
 
 import android.os.Bundle
 import android.view.View
+import android.view.ViewGroup
+import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
-import kotlinx.android.synthetic.main.research_layout.*
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 
 class ResearchActivity : AppCompatActivity() {
 
-    lateinit var iter: Iterator<View>
+    lateinit var recyclerView: RecyclerView
+    val list = mutableListOf<AppInfo>()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.research_layout)
-//        Storable.deleteFile(this, Storable.CUSTOM_GRID_APPS)
+
         AppManager.loadAllApps(this)
 
-/*        val allApps = AppManager.allApps
-        var i = 0
-        for (app in allApps) {
-            if (i > 12) break
-            AppManager.applyCustomGridChanges(this, i, app.key)
-            i++
-        }*/
-        layoutInflater.inflate(R.layout.research_merge, research_root, true)
+        val apps = AppManager.allApps.values.toList()
+        for (i in 0..15) {
+            list.add(i, apps[i])
+        }
+
+        recyclerView = findViewById(R.id.research_recycler_view)
+
+        recyclerView.adapter = object : RecyclerView.Adapter<BaseViewHolder>() {
+            override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): BaseViewHolder {
+                return BaseViewHolder(AppView(this@ResearchActivity, AppInfo("","")).apply {
+                    layoutParams = ViewGroup.LayoutParams(120,120)
+                })
+            }
+
+            override fun getItemCount() = list.size
+
+            override fun onBindViewHolder(holder: BaseViewHolder, position: Int) {
+                (holder.itemView as AppView).appInfo = list[position]
+            }
+        }
+
+        recyclerView.layoutManager = LinearLayoutManager(this, LinearLayoutManager.HORIZONTAL, false)
+
+        val btn = findViewById<Button>(R.id.research_btn)
+        btn.setOnClickListener {
+            list.removeAt(2)
+            recyclerView.adapter?.notifyItemMoved(2,5)
+        }
     }
 }
+
+class BaseViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView)
