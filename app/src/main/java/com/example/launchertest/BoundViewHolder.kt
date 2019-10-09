9a0618d0b09d776be4130649a0d45a7951464faf
Max <poldersen2@gmail.com>
2019-10-09 15:23:53 +0300

    project rebase

--- app/src/main/java/com/example/launchertest/BoundViewHolder.kt
+++
@@ -1,8 +0,0 @@
-package com.example.launchertest
-
-import android.view.View
-import androidx.recyclerview.widget.RecyclerView
-
-class BoundViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
-    var boundPosition = -1
-}
